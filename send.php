ca4186f290c97fe9f92653708ec4bb128fa27da3
Shadow777885 <olivierblondin777885@gmail.com>
2022-10-27 13:10:42 -0400

    Update send.php

--- send.php
+++ send.php
@@ -10,11 +10,8 @@
     $radio = htmlspecialchars($_POST['radio']);
 
     $conn = mysqli_connect($server, $username, $password, $database);
-    if (!$conn) {
-        echo("Connection failed: " . mysqli_connect_error())
-    }
  
-    $sql = "SELECT num_items FROM Products WHERE product_name={$radio};";
+    $sql = "SELECT num_items FROM Products WHERE product_name="{$radio}"";";
     $result = mysqli_query($conn, $sql);
     $row = mysql_fetch_row($result);
     foreach($result as $row) // There should only be one row returned 
