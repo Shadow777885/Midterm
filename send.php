c82819843fad18c8852d92392cab560e1cedd3ab
Shadow777885 <olivierblondin777885@gmail.com>
2022-10-27 13:04:47 -0400

    Update send.php

--- send.php
+++ send.php
@@ -14,12 +14,12 @@
         echo("Connection failed: " . mysqli_connect_error())
     }
  
-    $sql = "SELECT num_items, product_name FROM Products WHERE product_name={$radio};";
+    $sql = "SELECT num_items FROM Products WHERE product_name={$radio};";
     $result = mysqli_query($conn, $sql);
     $row = mysql_fetch_row($result);
     foreach($result as $row) // There should only be one row returned 
     { 
-        echo "There are {$row['num_items']} {$row['product_name']} in stock."; 
+        echo "There are {$row['num_items']} $radio in stock."; 
     }
     echo "Just A test"; 
     mysqli_close($conn);
