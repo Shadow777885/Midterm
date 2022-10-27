edd8d9a5c27bb7fc601c7077591e1982f6984e53
Shadow777885 <olivierblondin777885@gmail.com>
2022-10-27 14:11:25 -0400

    Changes for test

--- send.php
+++ send.php
@@ -7,7 +7,7 @@
     $username = "php";
     $password = "1.1.";
     $database = "Users";
-    $radio = htmlspecialchars($_POST['radio']);
+    $radio = htmlspecialchars($_GET['radio']);
 
     $conn = mysqli_connect($server, $username, $password, $database);
     if (!$conn) {
@@ -16,20 +16,20 @@
       $sql = "SELECT num_items FROM Products WHERE product_name={$radio};";
       echo $sql;
       $result = mysqli_query($conn, $sql);
+      echo $result
       ?>
 </head>
 
 <body>
     You selected: <?=$radio?>.<br/>
-    <p>
     <?php
         foreach($result as $row) // There should only be one row returned! 
         { 
-            echo "There are {$row['num_items']} $radio."; 
+            echo "There are {$row['num_items']} $radio.";
+            echo "test " $radio; 
         } 
     mysqli_close($conn);
     ?>
-    <p>
     <p> Test end <p>
 </body>
 </html>
