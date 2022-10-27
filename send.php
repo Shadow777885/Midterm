005c09088fb6096b880f42ae6dbeafdf3d828b59
Shadow777885 <olivierblondin777885@gmail.com>
2022-10-27 14:43:10 -0400

    Update send.php

--- send.php
+++ send.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <p> Test begin <p>
     <?php
     $server = "localhost";
     $username = "php";
@@ -14,7 +13,6 @@
         die("Connection failed: {mysqli_connect_error()}");
       }
       $sql = "SELECT num_items, product_name FROM Products WHERE product_name='{$radio}';";
-      echo $sql;
       $result = mysqli_query($conn, $sql);
       ?>
 </head>
@@ -22,13 +20,11 @@
 <body>
     You selected: <?=$radio?>.<br/>
     <?php
-        echo "test $radio.";
         foreach($result as $row) // There should only be one row returned!
         { 
             echo "There are {$row['num_items']} {$row['product_name']}.";
         } 
     mysqli_close($conn);
     ?>
-    <p> Test end <p>
 </body>
 </html>
